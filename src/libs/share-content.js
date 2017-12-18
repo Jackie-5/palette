686726e39e6ee3bf681873347e513861779c0bde
jackie.wu <jackie.wu@meituan.com>
2017-12-18 17:36:24 +0800

    test

--- src/libs/share-content.js
+++ src/libs/share-content.js
@@ -4,14 +4,14 @@
 
 export const shareIndex = {
     title: `[乙度抄经]`,
-    desc: '『乙东方 · 度千处』点亮一盏心灯，送出一份祝福。',
+    desc: '乙度抄经，抄录经典；明心见性，放飞心灯。',
     link: `http://wechat.eastdoing.com/chaojing/index.html`,
     imgUrl: 'http://wechat.eastdoing.com/chaojing/share.jpg'
 };
 
 export const shareName = {
     title: '的抄经作品',
-    desc: '抄录经典，明心见性；点亮心灯，送出祝福。',
+    desc: '乙度抄经，抄录经典；明心见性，放飞心灯。',
     link: 'http://wechat.eastdoing.com/chaojing/share.html',
     imgUrl: 'http://wechat.eastdoing.com/chaojing/share.jpg',
 };
